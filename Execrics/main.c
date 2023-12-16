d3c627a70955c2f09b34c4e43eea9434add041c3
louis <2354949236@qq.com>
2023-12-17 00:32:24 +0800

    feat: commit

--- Execrics/main.c
+++ Execrics/main.c
@@ -23,8 +23,14 @@ void TraverseList(pNode h)
    return;
 }
 
-pNode CreateLinkList(int arr[], int size)
+pNode CreateLinkList(int arr[])
 {
+   int size = 0;
+   while (arr[size] != -1)
+   {
+      size++;
+   }
+
    pNode newLinkList = (pNode)malloc(sizeof(Node));
    pNode tail = newLinkList;
    tail->next = NULL;
@@ -86,15 +92,21 @@ void MergeLinks(pNode la, pNode lb)
 
 int main()
 {
-   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 8};
-   // CreateLinkList(arr, sizeof(arr) / sizeof(arr[0]));
+   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 8, -1};
+   // CreateLinkList(arr);
 
    // 数据结构
    // 2.线性表
-   // 2.2.1 将两个递增的有序链表合并为 一个递增的有序链表。要求结果链表仍使用原来两个链表的存储空间，不另外占用其他的存储空间。表中不允许有重复的数据。
-   int arr11[] = {1, 2, 3, 4, 5, 7, 12};
-   int arr12[] = {2, 4, 5, 6, 9};
-   MergeLinks(CreateLinkList(arr11, 7), CreateLinkList(arr12, 5));
+   // 2.2.1 将两个递增的有序链表合并为 一个递增的有序链表。
+   // 要求结果链表仍使用原来两个链表的存储空间，不另外占用其他的存储空间。
+   // 表中不允许有重复的数据。
+   int arr11[] = {1, 2, 3, 4, 5, 7, 12, -1};
+   int arr12[] = {2, 4, 5, 6, 9, -1};
+   MergeLinks(CreateLinkList(arr11), CreateLinkList(arr12));
+
+   // 2.2.2 将两个非递减的有序链表合并为 一个非递增的有序链表。
+   // 要求结果链表仍使用原来两个链表的存储空间，不另外占用其他的存储空间。
+   // 表中允许有重复的数据。
 
    return 0;
 }
