8f922321c73edb51951691495b63bc9f064317f6
louis <2354949236@qq.com>
2023-12-17 00:23:25 +0800

    feat: commit MergeLinks

--- Execrics/main.c
+++ Execrics/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-// 数据结构
-// 线性表
 
-// 将两个递增的有序链表合并为 一个递增的有序链表。要求结果链表仍使用原来两个链表的存储空间，不另外占用其他的存储空间。表中不允许有重复的数据。
 typedef struct node
 {
    int data;          // 存储整数数据的成员
@@ -41,15 +38,63 @@ pNode CreateLinkList(int arr[], int size)
       tail = newNode;
    }
 
+   printf("create linklist: ");
    TraverseList(newLinkList);
 
    return newLinkList;
 }
 
+void MergeLinks(pNode la, pNode lb)
+{
+   pNode pa = la->next;
+   pNode pb = lb->next;
+   pNode pc = la; // important
+   pNode tail = pc;
+
+   while (pa && pb)
+   {
+      if (pa->data < pb->data)
+      {
+         // 取较小者La中的元素，将pa链接在pc的后面，pa指针后移
+         tail->next = pa;
+         tail = pa;
+         pa = pa->next;
+      }
+      else if (pa->data > pb->data)
+      {
+         // 取较小者Lb中的元素，将pb链接在pc的后面，pb指针后移
+         tail->next = pb;
+         tail = pb;
+         pb = pb->next;
+      }
+      else
+      {
+         // 相等时取La中的元素，删除Lb中的元素
+         tail->next = pa;
+         tail = pa;
+         pa = pa->next;
+
+         pNode temp = pb->next;
+         free(pb);
+         pb = temp;
+      }
+   }
+   tail->next = pa ? pa : pb;
+   printf("result: ");
+   TraverseList(pc);
+}
+
 int main()
 {
-   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 8, 8};
-   CreateLinkList(arr, sizeof(arr) / sizeof(arr[0]));
+   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 8};
+   // CreateLinkList(arr, sizeof(arr) / sizeof(arr[0]));
+
+   // 数据结构
+   // 2.线性表
+   // 2.2.1 将两个递增的有序链表合并为 一个递增的有序链表。要求结果链表仍使用原来两个链表的存储空间，不另外占用其他的存储空间。表中不允许有重复的数据。
+   int arr11[] = {1, 2, 3, 4, 5, 7, 12};
+   int arr12[] = {2, 4, 5, 6, 9};
+   MergeLinks(CreateLinkList(arr11, 7), CreateLinkList(arr12, 5));
 
    return 0;
 }
