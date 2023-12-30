25053c9d9aabb2bf241786a27cb4a9e1eb54729c
louis <2354949236@qq.com>
2023-12-30 23:24:09 +0800

    ready to w

--- Execrics/4-串、数组和广义表.cpp
+++ Execrics/4-串、数组和广义表.cpp
@@ -10,8 +10,17 @@ typedef int SElemType;
 
 // （1）写一个算法统计在输入字符串中各个不同字符出现的频度并将结果存入文件
 // （字符串中的合法字符为A-Z这26个字母和0-9这10个数字）。
+void ASCCount(const char str[])
+{
+    while (*str != '\0')
+    {
+        cout << *str++ << endl;
+    }
+}
 
 int main()
 {
+    const char str[][8] = {"sdrwr2", "342dfwf"};
+    ASCCount(str[1]);
     return 0;
 }
