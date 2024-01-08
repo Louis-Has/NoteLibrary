6a8929f59720215a5fcfc7adf9565e8209949025
louis <2354949236@qq.com>
2024-01-09 00:58:43 +0800

    feat: tree print

--- Execrics/5-树和二叉树.cpp
+++ Execrics/5-树和二叉树.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <optional>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ public:
     SElemType data;
     TreeNode *left;
     TreeNode *right;
+
     TreeNode(SElemType da) : data(da), left(nullptr), right(nullptr) {}
 };
 
@@ -22,14 +24,23 @@ class Tree
 private:
     TreeNode *root;
     TreeNode *Insert(TreeNode *current, SElemType da);
+    void InorderTraversal(TreeNode *current);
 
 public:
-    Tree() : root(nullptr) {}
+    Tree() : root(nullptr)
+    {
+    }
 
     void insert(SElemType da)
     {
         root = Insert(root, da);
     }
+
+    void printTree()
+    {
+        InorderTraversal(root);
+        cout << endl;
+    };
 };
 
 TreeNode *Tree::Insert(TreeNode *current, SElemType da)
@@ -51,21 +62,34 @@ TreeNode *Tree::Insert(TreeNode *current, SElemType da)
     return current;
 }
 
+void Tree::InorderTraversal(TreeNode *current)
+{
+    if (current != nullptr)
+    {
+        cout << current->data << " -> ";
+        InorderTraversal(current->left);
+        InorderTraversal(current->right);
+    }
+}
 // （1）统计二叉树的叶结点个数。
 // [题目分析]如果二叉树为空，
 // 返回0，如果二叉树不为空且左右子树为空，返回1，
 // 如果二叉树不为空，且左右子树不同时为空，返回左子树中叶子节点个数加上右子树中叶子节点个数。
 
-int LeafNodeCount(Tree T)
-{
-}
+// int LeafNodeCount(Tree T)
+// {
+// }
 
 int main()
 {
-    // 1
-    Tree Tr ;
-
-
+    // （1）统计二叉树的叶结点个数。
+    Tree Tr;
+    Tr.insert(0);
+    Tr.insert(2);
+    Tr.insert(5);
+    Tr.insert(3);
+    Tr.insert(1);
+    Tr.printTree();
 
     return 0;
 }
