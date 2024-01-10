b39e5fc668cc917d639fc75692a922ba037b23fd
louis <2354949236@qq.com>
2024-01-10 17:00:23 +0800

    feat: countLeaf

--- Execrics/5-树和二叉树.cpp
+++ Execrics/5-树和二叉树.cpp
@@ -22,14 +22,38 @@ public:
 class Tree
 {
 private:
-    TreeNode *root;
-    TreeNode *Insert(TreeNode *current, SElemType da);
-    void InorderTraversal(TreeNode *current);
+    TreeNode *Insert(TreeNode *current, SElemType da)
+    {
+        if (current == nullptr)
+        {
+            return new TreeNode(da);
+        }
+
+        if (da < current->data)
+        {
+            current->left = Insert(current->left, da);
+        }
+        else if (da > current->data)
+        {
+            current->right = Insert(current->right, da);
+        }
+
+        return current;
+    };
+
+    void InorderTraversal(TreeNode *current)
+    {
+        if (current != nullptr)
+        {
+            cout << current->data << " -> ";
+            InorderTraversal(current->left);
+            InorderTraversal(current->right);
+        }
+    };
 
 public:
-    Tree() : root(nullptr)
-    {
-    }
+    TreeNode *root;
+    Tree() : root(nullptr) {}
 
     void insert(SElemType da)
     {
@@ -38,58 +62,70 @@ public:
 
     void printTree()
     {
-        InorderTraversal(root);
-        cout << endl;
+        if (root == nullptr)
+        {
+            cout << "tree is null" << endl;
+        }
+        else
+        {
+            InorderTraversal(root);
+            cout << endl;
+        }
     };
+
+    //     void printTreeMap()
+    //     {
+    //         int rows = 3;
+    //         int cols = 5;
+
+    //         // new arrDate
+    //         vector<vector<char>> arrDate(rows, vector<char>(cols, '_'));
+
+    //         // show all date
+    //         for (size_t i = 0; i < rows; i++)
+    //         {
+    //             for (size_t j = 0; j < cols; j++)
+    //             {
+    //                 cout << arrDate[i][j] << " - ";
+    //             }
+    //             cout << endl;
+    //         }
+    //     };
 };
 
-TreeNode *Tree::Insert(TreeNode *current, SElemType da)
-{
-    if (current == nullptr)
-    {
-        return new TreeNode(da);
-    }
-
-    if (da < current->data)
-    {
-        current->left = Insert(current->left, da);
-    }
-    else if (da > current->data)
-    {
-        current->right = Insert(current->right, da);
-    }
-
-    return current;
-}
-
-void Tree::InorderTraversal(TreeNode *current)
-{
-    if (current != nullptr)
-    {
-        cout << current->data << " -> ";
-        InorderTraversal(current->left);
-        InorderTraversal(current->right);
-    }
-}
 // （1）统计二叉树的叶结点个数。
 // [题目分析]如果二叉树为空，
 // 返回0，如果二叉树不为空且左右子树为空，返回1，
 // 如果二叉树不为空，且左右子树不同时为空，返回左子树中叶子节点个数加上右子树中叶子节点个数。
 
-// int LeafNodeCount(Tree T)
-// {
-// }
+int LeafNodeCount(TreeNode *T)
+{
+    if (T == nullptr)
+    {
+        return 0;
+    }
+    else if (T->left == nullptr && T->right == nullptr)
+    {
+        return 1;
+    }
+
+    int res = LeafNodeCount(T->left) + LeafNodeCount(T->right);
+    return res;
+}
 
 int main()
 {
-    // （1）统计二叉树的叶结点个数。
     Tree Tr;
-    Tr.insert(0);
+    Tr.printTree();
+    Tr.insert(8);
     Tr.insert(2);
     Tr.insert(5);
     Tr.insert(3);
     Tr.insert(1);
     Tr.printTree();
+
+    // （1）统计二叉树的叶结点个数。
+    cout << LeafNodeCount(Tr.root) << endl;
 
     return 0;
 }
